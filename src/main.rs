679d3d73f7d5f64244be46817d95b91dad373362
AVB-Acess <vanbakeladam@gmail.com>
2023-05-15 14:03:28 -0400

    Fixed update_neighbours overwriting mines

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,34 @@ use std::io::{stdin, stdout, Write};
 use rand::Rng;
 use std::collections::HashSet;
 
+/**
+ * TODO: 
+ *  - Hide the field from user view
+ *  - Ask the user for spots to click
+ *  - Render the open spots
+ *  - Build the UI
+ * 
+ *  Could be nice:
+ *      - Only generate mines after first spot check
+ *      - Hide the mine locations in memory some how? (Could be way harder than nessiary for this scope)
+ */
+
+/**
+ * Dev ideas:
+ *  - Field is covered in ?s for text version
+ *  - Keep a second Vec to keep track of what the user has reveiled
+ *      - Fill with boolean 0/1 for visible/not
+ */
 fn main() {
     let (mut n, mut m) = (0u8,0u8);
 
     get_user_in(&mut n, &mut m);
     println!("Field sizes: {n}, {m}");
     let mut field: Vec<Vec<i8>> = vec![vec![0;n as usize];m as usize];
-
-    debug_print_field(&field);
-    println!("Now asking for mines...");
+    let mut _user_visible_field: Vec<Vec<bool>> = vec![vec![false;n as usize]; m as usize];
     let _no_mines = generate_mines(&mut field, n, m);
+
+    println!("-----------------------"); //Seperating debug print from above call
     debug_print_field(&field);
 }
 
@@ -27,7 +45,7 @@ fn main() {
  */
 fn generate_mines(field: &mut Vec<Vec<i8>>, n: u8, m: u8) -> u8 {
     let mut set_mines: HashSet<(u8,u8)> = HashSet::new();
-    let no_mines = ask_user_mines_no();
+    let no_mines = ask_user_mines_no(); // TODO move this to main?
     let mut curr_mines = 0;
     let mut rng = rand::thread_rng();
 
@@ -44,6 +62,8 @@ fn generate_mines(field: &mut Vec<Vec<i8>>, n: u8, m: u8) -> u8 {
         }else { continue; /*try again*/ } 
     }
 
+    debug_print_field_raw(&field);
+
     return no_mines;
 }
 
@@ -51,14 +71,22 @@ fn generate_mines(field: &mut Vec<Vec<i8>>, n: u8, m: u8) -> u8 {
  * Increment all the neighbours of a new mine by one
  */
 fn update_neighbours(field: &mut Vec<Vec<i8>>, m: u8, n: u8, point: &(u8,u8)) {
-    if point.0 > 0 && point.1 > 0 { field[(point.0-1) as usize][(point.1-1) as usize] += 1; }       /* NW */
-    if point.0 > 0 { field[(point.0-1) as usize][point.1 as usize] += 1; }                          /* N */
-    if point.0 > 0 && point.1+1 < n { field[(point.0-1) as usize][(point.1+1) as usize] += 1; }     /* NE */
-    if point.1 > 0 { field[point.0 as usize][(point.1-1) as usize] += 1; }                          /* W */ 
-    if point.1+1 < n { field[point.0 as usize][(point.1+1) as usize] += 1; }                        /* E */
-    if point.0+1 < m && point.1 > 0 { field[(point.0+1) as usize][(point.1-1) as usize] += 1; }     /* SW */
-    if point.0+1 < m { field[(point.0+1) as usize][point.1 as usize] += 1;}                         /* S */
-    if point.0+1 < m && point.1+1 < n { field[(point.0+1) as usize][(point.1+1) as usize] += 1; }   /* SE */
+    if (point.0 > 0 && point.1 > 0) && field[(point.0-1) as usize][(point.1-1) as usize] != -1 
+        { field[(point.0-1) as usize][(point.1-1) as usize] += 1; }                         /* NW */
+    if point.0 > 0 && field[(point.0-1) as usize][point.1 as usize] != -1 
+        { field[(point.0-1) as usize][point.1 as usize] += 1; }                             /* N */
+    if (point.0 > 0 && point.1+1 < n) && field[(point.0-1) as usize][(point.1+1) as usize] != -1
+        { field[(point.0-1) as usize][(point.1+1) as usize] += 1; }                         /* NE */
+    if point.1 > 0 && field[point.0 as usize][(point.1-1) as usize] != -1
+        { field[point.0 as usize][(point.1-1) as usize] += 1; }                             /* W */ 
+    if point.1+1 < n && field[point.0 as usize][(point.1+1) as usize] != -1
+        { field[point.0 as usize][(point.1+1) as usize] += 1; }                             /* E */
+    if (point.0+1 < m && point.1 > 0) && field[(point.0+1) as usize][(point.1-1) as usize] != -1
+        { field[(point.0+1) as usize][(point.1-1) as usize] += 1; }                         /* SW */
+    if point.0+1 < m && field[(point.0+1) as usize][point.1 as usize] != -1
+        { field[(point.0+1) as usize][point.1 as usize] += 1;}                              /* S */
+    if (point.0+1 < m && point.1+1 < n) && field[(point.0+1) as usize][(point.1+1) as usize] != -1
+        { field[(point.0+1) as usize][(point.1+1) as usize] += 1; }                         /* SE */
 }
 
 
@@ -84,6 +112,25 @@ fn debug_print_field(field: &Vec<Vec<i8>>) {
     for (_i, row) in field.iter().enumerate() {
         for (_j, colmn) in row.iter().enumerate() {
             if *colmn == -1 { print!("X "); }
+            else { print!("{} ", colmn); } 
+        }
+        println!();
+    }
+}
+
+fn debug_print_field_raw(field: &Vec<Vec<i8>>) {
+    for (_i, row) in field.iter().enumerate() {
+        for (_j, colmn) in row.iter().enumerate() {
+            print!("{} ", colmn);
+        }
+        println!();
+    }
+}
+
+fn _debug_print_user_field(field: &Vec<Vec<bool>>) {
+    for (_i, row) in field.iter().enumerate() {
+        for (_j, colmn) in row.iter().enumerate() {
+            if !*colmn { print!("? "); }
             else { print!("{} ", colmn); } 
         }
         println!();
